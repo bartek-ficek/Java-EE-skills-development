66b6927ed62b42f2960c7bfd91e246e0894b127c
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-08 10:42:19 +0100

    POJO class Todo created

---
+++ hello-todo/src/main/java/academy/learnprogramming/entity/Todo.java
@@ -0,0 +1,52 @@
+package academy.learnprogramming.entity;
+
+import java.time.LocalDate;
+
+public class Todo {
+
+    private String task;
+    private LocalDate dueDate;
+    private boolean isCompleted;
+    private LocalDate dateCompleted;
+    private LocalDate dateCreated;
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public LocalDate getDateCompleted() {
+        return dateCompleted;
+    }
+
+    public void setDateCompleted(LocalDate dateCompleted) {
+        this.dateCompleted = dateCompleted;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
