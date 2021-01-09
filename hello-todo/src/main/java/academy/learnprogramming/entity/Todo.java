bdbec4eb167143d12bf50fd9f021c03b8f02eb4d
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-09 08:39:47 +0100

    Validation implemented in Todo entity

--- hello-todo/src/main/java/academy/learnprogramming/entity/Todo.java
+++ hello-todo/src/main/java/academy/learnprogramming/entity/Todo.java
@@ -1,6 +1,11 @@
 package academy.learnprogramming.entity;
 
+import javax.json.bind.annotation.JsonbDateFormat;
 import javax.persistence.*;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Entity
@@ -9,10 +14,20 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotEmpty(message = "Task must be set")
+    @Size(min=10, message = "Task should not be less than 10 characters")
     private String task;
+
+    @NotNull(message = "Due must be set")
+    @FutureOrPresent(message = "Due must be in the present or the future")
+    @JsonbDateFormat(value = "yyyy-MM-dd")
     private LocalDate dueDate;
+
     private boolean isCompleted;
+
     private LocalDate dateCompleted;
+
     private LocalDate dateCreated;
 
     @PrePersist
